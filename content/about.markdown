cd655ae538915cb765e539aa47d5d6836badd33a
rudi <rudiwillalwaysloveyou@gmail.com>
2018-05-28 01:00:06 +0430

    Integrated withering-spell

--- content/about.markdown
+++ content/about.markdown
@@ -1,2 +1,3 @@
 # about
 I am just trying to be a site. Did you know when I was created this address was shared with few people?
+
--- src/site/core.clj
+++ src/site/core.clj
@@ -7,11 +7,18 @@
 <head>
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <link href=\"./css/style.css\" rel=\"stylesheet\" type=\"text/css\">
+    <link href=\"./css/raw_style.css\" rel=\"stylesheet\" type=\"text/css\">
     <style>
        @import url('https://fonts.googleapis.com/css?family=Roboto:100i');
     </style>
 </head>  
 "
-            [:div {:style "max-width: 900px; margin: 40px auto;"}
-             (-> data :entry :content)
-             (-> "resources/footer.markdown" m/file->hiccup m/component)]))
+            [:body [:div {:style "max-width: 900px; margin: 40px auto;"}
+                    (-> data :entry :content)
+                    [:div {:id "app"}]
+                    [:canvas {:id "hello-quil"}]
+                    (-> "resources/footer.markdown" m/file->hiccup m/component)]
+             [:script {:src "/js/compiled/withering_spell.js" :type "text/javascript"}]
+             "<script type=\"text/javascript\">withering_spell.system.go();</script>"
+             "    <script type=\"text/javascript\">withering_spell.quilcore.run_sketch()</script>"]))
