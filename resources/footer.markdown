84bd49a96030646316bd83d42761c1fe634c2a8f
rudi <rudiwillalwaysloveyou@gmail.com>
2018-05-28 00:09:09 +0430

    It's our first graceful site *_*

--- resources/footer.markdown
+++ resources/footer.markdown
@@ -2,5 +2,5 @@
 [home](./index.html) [about](./about.html) [don't click on me](./boring.html)
 
 <p style="text-align: center; color: gray;font-family: 'Roboto', sans-serif; font-weight: 100; font-style: italic;">
-I was written by Fereidoon Mehri in 2018. They tell me with love. 💙
+I was written by Fereidoon Mehri in 2018. They tell me, "With love. 💙"
 </p>
--- src/site/core.clj
+++ src/site/core.clj
@@ -3,10 +3,15 @@
   (:require [markdown-to-hiccup.core :as m]))
 
 (defn page [data]
-  (hp/html5 "<meta charset=\"UTF-8\">
-<style>
-@import url('https://fonts.googleapis.com/css?family=Roboto:100i');
-</style>"
+  (hp/html5 "
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <style>
+       @import url('https://fonts.googleapis.com/css?family=Roboto:100i');
+    </style>
+</head>  
+"
             [:div {:style "max-width: 900px; margin: 40px auto;"}
              (-> data :entry :content)
              (-> "resources/footer.markdown" m/file->hiccup m/component)]))
